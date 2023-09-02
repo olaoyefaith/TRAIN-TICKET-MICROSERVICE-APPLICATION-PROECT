1cc3427f2848095eab43fa5b236be45adc07c723
olaoye faith <olaoyefaith1@gmail.com>
2023-09-02 18:35:56 +0100

    modified terraform infrastructure

---
+++ infra/variables.tf
@@ -0,0 +1,75 @@
+variable "node_count" {
+  default = 1
+}
+
+variable "dns_prefix" {
+  default = "aks-k8s-2022"
+
+}
+
+variable "cluster_name" {
+
+}
+
+variable "kubernetes_version" {
+
+}
+
+# variable "acr_name" {
+
+# }
+
+# variable "sql_name" {
+
+# }
+
+# variable "db_name" {
+
+# }
+
+# variable "db_admin_login" {
+
+# }
+
+# variable "db_admin_password" {
+
+# }
+
+# variable "storage_name" {
+
+# }
+
+variable "resource_group_name" {
+  description = "Name of the Azure resource group"
+  type        = string
+
+}
+
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+  default     = "westeurope"
+}
+
+
+variable "administrator_login" {
+  description = "Administrator username for PostgreSQL"
+  type        = string
+}
+
+variable "administrator_login_password" {
+  description = "Administrator password for PostgreSQL"
+  type        = string
+}
+           
+
+# variable "pg_server_name" {
+#   description = "Name of the PostgreSQL server"
+#   type        = string
+# }
+
+# variable "environment_tag" {
+#   description = "Tag to identify the environment"
+#   type        = string
+#   default     = "dev"
+# }
