1a0ed3ecaf5b6c79c51121dd51fc6d29e118c11f
olaoye faith <olaoyefaith1@gmail.com>
2023-10-02 18:13:47 +0100

    authenticate to terraform using oidc

--- infra/main.tf
+++ infra/main.tf
@@ -4,6 +4,22 @@ resource "azurerm_resource_group" "rg" {
 }
 
 
+data "azuread_client_config" "current" {}
+
+resource "azuread_application" "example" {
+  display_name = "example"
+  owners       = [data.azuread_client_config.current.object_id]
+}
+
+resource "azuread_service_principal" "example" {
+  application_id               = azuread_application.example.application_id
+  app_role_assignment_required = false
+  owners                       = [data.azuread_client_config.current.object_id]
+  
+  }
+
+
+
 resource "azurerm_kubernetes_cluster" "k8s" {
   name                = var.cluster_name
   location            = azurerm_resource_group.rg.location
