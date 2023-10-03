6f9424ab48267058d3d1c3e83b658b5218ff76c9
olaoye faith <olaoyefaith1@gmail.com>
2023-10-03 16:02:56 +0100

    authenticate terraform and resource creation

--- infra/provider.tf
+++ infra/provider.tf
@@ -5,8 +5,6 @@
 
 provider "azurerm" {
    subscription_id = var.SUBSCRIPTION_ID
-   client_id       = var.SP_CLIENT_ID
-   client_secret   = var.SP_CLIENT_SECRET
    tenant_id       = var.SP_TENANT_ID
    use_oidc = true
   features {}
