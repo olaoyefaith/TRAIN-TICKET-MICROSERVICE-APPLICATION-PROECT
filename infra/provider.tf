07c34883a44d52036d9ff92fb96dc9e9f8f3876b
olaoye faith <olaoyefaith1@gmail.com>
2023-10-03 15:12:43 +0100

    authenticate terraform and reesource creation

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,8 +1,14 @@
+variable "oidc_token" {}
+variable "oidc_token_file_path" {}
+variable "oidc_request_token" {}
+variable "oidc_request_url" {}
+
 provider "azurerm" {
    subscription_id = var.SUBSCRIPTION_ID
    client_id       = var.SP_CLIENT_ID
    client_secret   = var.SP_CLIENT_SECRET
    tenant_id       = var.SP_TENANT_ID
+   use_oidc = true
   features {}
 }
 
@@ -14,3 +20,7 @@ terraform {
     }
   }
 }
+
+
+oidc_request_token = var.oidc_request_token
+  oidc_request_url   = var.oidc_request_url
