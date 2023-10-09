5d0128e2283cd4828db8b1be7c76bab9c1a554ab
Olaoye faith <olaoyefaith1@gmail.com>
2023-10-09 16:53:35 +0100

    merge kubernete manifest branch to main

---
+++ infra/output.tf
@@ -0,0 +1,18 @@
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config_raw
+  sensitive = true
+}
+
+output "host" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config.0.host
+  sensitive = true
+}
+
+output "aks_identity" {
+  value = azurerm_kubernetes_cluster.k8s.identity
+}
+
+
+# output "pg_connection_string" {
+#   value = azurerm_postgresql_server.example_pg.connection_strings["psql"]
+# }
---
+++ infra/variables.tf
@@ -0,0 +1,107 @@
+variable "node_count" {
+  default = 1
+}
+
+variable "dns_prefix" {
+  default = "aks-k8s-2022"
+
+}
+
+variable "cluster_name" {
+
+}
+
+variable "kubernetes_version" {
+
+}
+
+# variable "acr_name" {
+
+# }
+
+# variable "sql_name" {
+
+# }
+
+# variable "db_name" {
+
+# }
+
+# variable "db_admin_login" {
+
+# }
+
+# variable "db_admin_password" {
+
+# }
+
+# variable "storage_name" {
+
+# }
+
+variable "resource_group_name" {
+  description = "Name of the Azure resource group"
+  type        = string
+
+}
+
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+  default     = "westeurope"
+}
+
+# variable "aks_service_principal_client_id" {
+#   description = "Client ID of the Azure service principal for AKS"
+#   type        = string
+# }
+
+# variable "aks_service_principal_client_secret" {
+#   description = "Client Secret of the Azure service principal for AKS"
+#   type        = string
+# }
+
+variable "SUBSCRIPTION_ID" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "SP_CLIENT_ID" {
+  description = "Service Principal Client ID"
+  type        = string
+}
+
+variable "SP_CLIENT_SECRET" {
+  description = "Service Principal Client Secret"
+  type        = string
+}
+
+variable "SP_TENANT_ID" {
+  description = "Service Principal Tenant ID"
+  type        = string
+}
+
+
+
+
+# variable "administrator_login" {
+#   description = "Administrator username for PostgreSQL"
+#   type        = string
+# }
+
+# variable "administrator_login_password" {
+#   description = "Administrator password for PostgreSQL"
+#   type        = string
+# }
+           
+
+# variable "pg_server_name" {
+#   description = "Name of the PostgreSQL server"
+#   type        = string
+# }
+
+# variable "environment_tag" {
+#   description = "Tag to identify the environment"
+#   type        = string
+#   default     = "dev"
+# }
